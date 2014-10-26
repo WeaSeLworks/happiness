9ced53b8b956207c01113ca6b651362bbe2c767b
steve <steven@skone.net>
2014-10-26 15:31:40 +0000

    Updated grade properties

--- src/main/java/com/github/weaselworks/happiness/twitter/SentimentAnalyser.java
+++ src/main/java/com/github/weaselworks/happiness/twitter/SentimentAnalyser.java
@@ -1,21 +1,26 @@
 package com.github.weaselworks.happiness.twitter;
 
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.eventbus.Message;
-import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 import uk.bl.wa.sentimentalj.Sentiment;
 import uk.bl.wa.sentimentalj.SentimentalJ;
 
 public class SentimentAnalyser extends Verticle {
 
-    Logger logger = container.logger();
+    Logger logger = LoggerFactory.getLogger(SentimentAnalyser.class);
+
+
 
     @Override
     public void start() {
-        EventBus eb = vertx.eventBus();
 
+        EventBus eb = vertx.eventBus();
 
         Handler<Message> sentimentHandler = new Handler<Message>() {
             public void handle(Message message) {
@@ -24,6 +29,7 @@ public class SentimentAnalyser extends Verticle {
                 Sentiment s = sj.positivity(message.body().toString());
 
                 //write to event bus here.
+
 
             }
         };
