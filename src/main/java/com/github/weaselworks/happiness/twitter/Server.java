c602a95ff53b72bacbdaac2eae8f1dd00027ac99
Nick Lehane <nick@ntl-consulting.co.uk>
2014-10-27 22:20:36 +0000

    Server tinkering
    Added index.html and modified Server.java to use SockJS.  Removed
    mod-socket-io references from build.grade

--- src/main/java/com/github/weaselworks/happiness/twitter/Server.java
+++ src/main/java/com/github/weaselworks/happiness/twitter/Server.java
@@ -1,12 +1,15 @@
 package com.github.weaselworks.happiness.twitter;
 
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.sockjs.SockJSServer;
+import org.vertx.java.core.sockjs.SockJSSocket;
 import org.vertx.java.platform.Verticle;
-import com.nhncorp.mods.socket.io.SocketIOServer;
-import com.nhncorp.mods.socket.io.SocketIOSocket;
-import com.nhncorp.mods.socket.io.impl.DefaultSocketIOServer;
 
 /**
  * Created by nick on 25/10/2014.
@@ -16,35 +19,50 @@ public class Server extends Verticle {
     @Override
     public void start() {
 
+        final EventBus eb = vertx.eventBus();
+        eb.registerHandler("com.github.weaselworks.happiness.twitter.server", new Handler<Message<JsonObject>>() {
+
+            @Override
+            public void handle(Message<JsonObject> event) {
+
+                JsonObject o = (JsonObject) event.body();
+
+
+
+            }
+        });
+
         int port = 9090;
         HttpServer server = vertx.createHttpServer();
-        SocketIOServer io = new DefaultSocketIOServer(vertx, server);
 
-        io.sockets().onConnection(new Handler<SocketIOSocket>() {
+        server.requestHandler(new Handler<HttpServerRequest>() {
 
-            public void handle(final SocketIOSocket socket) {
+            @Override
+            public void handle(HttpServerRequest req) {
 
-                socket.on("timer", new Handler<JsonObject>() {
+                // Serve up the index.html
+                if (req.path().equals("/")) req.response().sendFile("src/main/resources/web/index.html");
 
-                    public void handle(JsonObject event) {
-                        socket.emit("timer", event);
+            }
+
+        });
+
+        // Start SockJSSever
+        SockJSServer sockServer = vertx.createSockJSServer(server);
+
+        sockServer.installApp(new JsonObject().putString("prefix", "/happiness"), new Handler<SockJSSocket>() {
+
+            @Override
+            public void handle(final SockJSSocket event) {
+                event.dataHandler(new Handler<Buffer>() {
+
+                    @Override
+                    public void handle(Buffer data) {
+
+                        // Echo the message back to the client
+                        event.write(data);
                     }
-
                 });
-
-                // Define behaviour for 'start tweets' channel
-                socket.on("start tweets", new Handler<JsonObject>() {
-
-                    public void handle(JsonObject event) {
-
-                        System.out.println("Start Tweets received");
-
-                        // Placeholder for Twitter Stream initialisation?
-
-                    }
-
-                });
-
 
             }
 
