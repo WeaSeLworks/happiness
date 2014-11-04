efa47e8c2bee2cdc3d6757a6d7d7f50a6ef992c3
Nick Lehane <nick@ntl-consulting.co.uk>
2014-11-04 21:35:43 +0000

    Changed heat map back to Google maps, improved performance on client

--- src/main/java/com/github/weaselworks/happiness/twitter/Server.java
+++ src/main/java/com/github/weaselworks/happiness/twitter/Server.java
@@ -39,25 +39,28 @@ public class Server extends Verticle {
             @Override
             public void handle(HttpServerRequest req) {
 
+                String path = req.path();
+
                 // Serve up the index.html
-                if (req.path().equals("/")) {
+                if (path.equals("/small-dot-icon.png")) {
+                    System.out.println("Dot requested");
+                    req.response().sendFile("web/small-dot-icon.png");
+                }
+
+                else if (path.equals("/")) {
                     req.response().sendFile("web/index.html");
                 }
 
                 // Should this really need to be here?!?
-                else if (req.path().equals("/vertxbus-2.1.js")) {
+                else if (path.equals("/vertxbus-2.1.js")) {
                     req.response().sendFile("web/vertxbus-2.1.js");
                 }
 
-                else if (req.path().equals("/small-dot-icon.png")) {
-                    req.response().sendFile("web/small-dot-icon.png");
-                }
-
-                else if (req.path().equals("/gmaps-heatmap.js")) {
+                else if (path.equals("/gmaps-heatmap.js")) {
                     req.response().sendFile("web/gmaps-heatmap.js");
                 }
 
-                else if (req.path().equals("/heatmap.js")) {
+                else if (path.equals("/heatmap.js")) {
                     req.response().sendFile("web/heatmap.js");
                 }
 
